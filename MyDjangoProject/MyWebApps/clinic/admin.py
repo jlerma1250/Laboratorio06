d9d2b37729288930d83458010dcf0db5b2f3ceb4
jlerma1250 <jlerma@unsa.edu.pe>
2026-05-28 15:41:16 -0500

    admin.py

--- MyDjangoProject/MyWebApps/clinic/admin.py
+++ MyDjangoProject/MyWebApps/clinic/admin.py
@@ -1,2 +1,19 @@
-from django.contrib import admin
+from .models import (
+    Specialty,
+    Doctor,
+    Patient,
+    Appointment,
+    MedicalRecord,
+    Treatment,
+    Exam,
+    Payment
+)
 
+admin.site.register(Specialty)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(MedicalRecord)
+admin.site.register(Treatment)
+admin.site.register(Exam)
+admin.site.register(Payment)
